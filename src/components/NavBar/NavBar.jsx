89ce206184028b3e6df88bfdd5e8ab7504845bde
Martin Counago <martincounago97@gmail.com>
2025-11-01 14:22:12 -0300

    Modificaciones en item y en contenedor

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,12 +4,12 @@ import CartWidget from "../CartWidget/CartWidget.jsx";
 const NavBar = () => {
   return (
     <header>
-      <h1>App</h1>
+      <h1>Medialunas calentitas</h1>
       <nav>
         <ul>
-          <li>Profesores</li>
-          <li>Estudiantes</li>
-          <li>Clases</li>
+          <li>Promociones</li>
+          <li>Dulces</li>
+          <li>Salados</li>
         </ul>
       </nav>
       <CartWidget />
